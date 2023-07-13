09f66c1a421a0913a13d08e38f0adba34086b62b
Theros <theros@svaltek.xyz>
2023-07-13 10:59:29 +0100

    general cleanup

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -15,23 +15,60 @@
 // You should have received a copy of the GNU General Public License
 // along with therosin.github.io.  If not, see <http://www.gnu.org/licenses/>.
 
-export default async function fetchAPI(url: string) {
+
+
+/**
+ * Fetches data from the API
+ * @param url The URL to fetch from
+ * @param options The options to pass to fetch
+ * @param asJson Whether to parse the response as JSON
+ * @returns The response from the API as a string or object
+ */
+export async function fetchAPI(url: string, options?: RequestInit): Promise<Response> {
+	
 	const headers = { 'User-Agent': 'chrome' };
 
 	try {
-		let response = await fetch(url, { headers });
-		let text = await response.text();
-		try {
-			if (text === null) {
-				return { error: 'Not found' };
-			}
-			return JSON.parse(text);
-		} catch (e) {
-			console.error(`Recevied from API: ${text}`);
-			console.error(e);
-			return { error: e };
+		let response = await fetch(url, { headers, ...options });
+		if (response.ok) {
+			return response;
+		} else {
+			throw new Error(response.statusText);
 		}
-	} catch (error) {
-		return { error };
+	} catch (error: any) {
+		throw new Error(`Error fetching ${url}: ${error.message}`);
 	}
+}
+
+/**
+ * Posts data to the API
+ * @param url The URL to post to
+ * @param data The data to post
+ * @param options The options to pass to fetch
+ * @param asJson Whether to parse the response as JSON
+ * @returns The response from the API as a string or object
+ * @throws Error if the response is not ok
+ * @throws Error if the response is not JSON
+*/
+export async function postAPI(url: string, data: any, options?: RequestInit): Promise<Response> {
+	
+	const headers = { 'User-Agent': 'chrome' };
+
+	try {
+		let response = await fetch(url, { headers, method: 'POST', body: JSON.stringify(data), ...options });
+		if (response.ok) {
+			return response;
+		} else {
+			throw new Error(response.statusText);
+		}
+	} catch (error: any) {
+		throw new Error(`Error posting to ${url}: ${error.message}`);
+	}
+}
+
+export default function useAPI(url: string, options?: RequestInit) {
+	return {
+		fetch: (path: string, opt?: RequestInit) => fetchAPI(url + path, { ...options, ...opt }),
+		post: (path: string, data: any, opt?: RequestInit) => postAPI(url + path, data, { ...options, ...opt })
+	};
 }
